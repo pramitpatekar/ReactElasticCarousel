8239cbcb3cbc6194692472aef5ffd0c2fa78c87c
pramitpatekar <77798733+pramitpatekar@users.noreply.github.com>
2023-10-02 23:48:18 +0530

    React-Elastic-Carousel Demo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import App from './App';
+//import reportWebVitals from './reportWebVitals';
+
+import Carousel from "react-elastic-carousel";
+import Item from "./Item";
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1},
+  { width: 550, itemsToShow: 2},
+  { width: 768, itemsToShow: 3},
+  { width: 1200, itemsToShow: 4},
+];
+
+function App() {
+  return(
+    <>
+      <h1 style={{ textAlign: "center"}}>Example to setup your Carousel in React</h1>
+      <div className='App'>
+        <Carousel breakPoints={breakPoints}>
+          <Item>One</Item>
+          <Item>Two</Item>
+          <Item>Three</Item>
+          <Item>Four</Item>
+          <Item>Five</Item>
+          <Item>Six</Item>
+          <Item>Seven</Item>
+          <Item>Eight</Item>
+        </Carousel>
+      </div>
+    </>
+  );
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +42,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
